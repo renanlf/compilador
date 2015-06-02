2f498e6052299e8eeb1479d67edf00273a35cfe2
renanlf <leandrorenanf@gmail.com>
2015-06-02 17:36:24 -0300

    GUI

--- test/edu/br/ufrpe/uag/compiler/analyzers/Program.java
+++ test/edu/br/ufrpe/uag/compiler/analyzers/Program.java
@@ -1,19 +1,6 @@
 package edu.br.ufrpe.uag.compiler.analyzers;
 
 public class Program {
-	static int multiplica(int a, int b) {
-		int c;
-		while (b > 1) {
-			a = a + a;
-			b = b - 1;
 
-		}
-		c = b + 1;
-		return (a);
 
-	}
-
-	public static void main(String args[]) {
-		System.out.println(multiplica(3, 2));
-	}
 }
